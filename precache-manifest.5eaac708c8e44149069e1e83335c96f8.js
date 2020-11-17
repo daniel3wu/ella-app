f6cf0f34ed7a81ed8f658c81e1e98ed3be3559f0
Danny Wu <daniel3wu@gmail.com>
2020-11-17 13:18:36 -0500

    Updates

---
+++ precache-manifest.5eaac708c8e44149069e1e83335c96f8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65cb9b22fbfb8fc4bc8e43be752b5e2",
+    "url": "/ella-app/index.html"
+  },
+  {
+    "revision": "cc29d4a0785617b28e5a",
+    "url": "/ella-app/static/css/2.3a0265c7.chunk.css"
+  },
+  {
+    "revision": "3393da32dfd2ccd1dc3a",
+    "url": "/ella-app/static/css/main.7dfaede0.chunk.css"
+  },
+  {
+    "revision": "cc29d4a0785617b28e5a",
+    "url": "/ella-app/static/js/2.e3e67107.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/ella-app/static/js/2.e3e67107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3393da32dfd2ccd1dc3a",
+    "url": "/ella-app/static/js/main.15dd8acf.chunk.js"
+  },
+  {
+    "revision": "66a96719e16ef0c0efb2",
+    "url": "/ella-app/static/js/runtime-main.bc718ba8.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ella-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ella-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ella-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ella-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ella-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ella-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ella-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ella-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ella-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ella-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ella-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ella-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ella-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ella-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ella-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ella-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
